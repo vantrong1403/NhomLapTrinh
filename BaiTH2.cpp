e9085263a4f166bec30672754e98edd9b9bdfa38
Phát <72639627+PhatNguyen-K61@users.noreply.github.com>
2021-05-17 22:06:40 +0700

    sửa lại rùi nè

--- BaiTH2.cpp
+++ BaiTH2.cpp
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <conio.h>
-void Nhap(int *a, int sophantu);
+void Nhap(int *a, int &sophantu);
 void Xuat(int *a, int sophantu);
 int main(){
     int *a;
     int sophantu;
+    a = (int*)malloc(sophantu*sizeof(int)); //cấp ở đây nè
     Nhap(a, sophantu);
     printf("\nMang vua nhap: ");
     Xuat(a, sophantu);
     free(a);
 }
-void Nhap(int *a, int sophantu){
+void Nhap(int *a, int &sophantu){ // thiếu tham chiếu
     do{
         printf("\nNhap vao so phan tu: ");
         scanf("%d", &sophantu);
     }while(sophantu < 0);
-    a = (int*) malloc(sophantu * sizeof(int));
+    //a = (int*) malloc(sophantu * sizeof(int));
     for(int index = 0; index < sophantu; index++){
         printf("\nNhap a[%d]: ", index+1);
-        scanf("%f", &*(a+index));
+        scanf("%d", &*(a+index));
     }
     printf("\nMang co %d phan tu", sophantu);
 }
 void Xuat(int *a, int sophantu){
     for(int index = 0; index < sophantu; index++){
-        printf("\na[%d]: %f", index+1, *(a+index));
+        printf("\na[%d]: %d", index+1, *(a+index));
     }
 }
